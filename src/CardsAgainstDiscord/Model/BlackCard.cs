37c58fc4e9b23b39c0499314137f6fcbb672dd57
Jiří Vrba <jiri@vrba.dev>
2022-02-06 18:56:43 +0100

    Remove custom property names

--- src/CardsAgainstDiscord/Model/BlackCard.cs
+++ src/CardsAgainstDiscord/Model/BlackCard.cs
@@ -3,25 +3,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CardsAgainstDiscord.Model;
 
-[Table("black_cards")]
 public class BlackCard
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    [Column("id")]
     public int Id { get; set; }
 
     /// <summary>
     /// Text of the black card
     /// </summary>
     [Required]
-    [Column("text")]
     public string Text { get; set; } = null!;
 
     /// <summary>
     /// Number of white cards required to complete this black card
     /// </summary>
     [Required]
-    [Column("picks")]
     public int Picks { get; set; } = 0;
 }
