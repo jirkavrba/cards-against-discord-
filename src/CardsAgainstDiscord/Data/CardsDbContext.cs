fa363abcf0f047133726365c9f8de897bb236098
Jiří Vrba <jiri@vrba.dev>
2022-02-06 18:36:38 +0100

    Add migration for black and white card tables

--- src/CardsAgainstDiscord/Data/CardsDbContext.cs
+++ src/CardsAgainstDiscord/Data/CardsDbContext.cs
@@ -1,3 +1,4 @@
+using CardsAgainstDiscord.Model;
 using Microsoft.EntityFrameworkCore;
 
 namespace CardsAgainstDiscord.Data;
@@ -7,4 +8,8 @@ public class CardsDbContext : DbContext
     public CardsDbContext(DbContextOptions<CardsDbContext> options) : base(options)
     {
     }
+
+    public virtual DbSet<BlackCard> BlackCards { get; protected set; } = null!;
+
+    public virtual DbSet<WhiteCard> WhiteCards { get; protected set; } = null!;
 }
---
+++ src/CardsAgainstDiscord/Migrations/20220206173138_WhiteBlackCards.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CardsAgainstDiscord.Migrations
+{
+    public partial class WhiteBlackCards : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "black_cards",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    text = table.Column<string>(type: "text", nullable: false),
+                    picks = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_black_cards", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "white_cards",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    text = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_white_cards", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "black_cards");
+
+            migrationBuilder.DropTable(
+                name: "white_cards");
+        }
+    }
+}
