fb3108206f3c49e3d4d874e6d3a88fdae37626c5
Jiří Vrba <jiri@vrba.dev>
2022-02-06 23:07:56 +0100

    Implement updating lobby embeds

--- src/CardsAgainstDiscord/Services/LobbiesService.cs
+++ src/CardsAgainstDiscord/Services/LobbiesService.cs
@@ -1,8 +1,8 @@
 using CardsAgainstDiscord.Data;
 using CardsAgainstDiscord.Exceptions;
-using CardsAgainstDiscord.Migrations;
 using CardsAgainstDiscord.Model;
 using CardsAgainstDiscord.Services.Contracts;
+using Discord;
 using Discord.WebSocket;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +19,15 @@ public class LobbiesService : ILobbiesService
         _factory = factory;
         _client = client;
     }
+
+    private Embed LobbyCancelledEmbed => new EmbedBuilder
+        {
+            Title = "Game cancelled by the owner",
+            Description = "To create a new game, please use the **/create-game** slash command",
+            ThumbnailUrl = "https://i.imgur.com/JbVGbzw.png"
+        }
+        .WithCurrentTimestamp()
+        .Build();
 
     public async Task<Lobby> CreateLobbyAsync(ulong guildId, ulong channelId, ulong messageId, ulong ownerId)
     {
@@ -62,7 +71,7 @@ public class LobbiesService : ILobbiesService
     public async Task LeaveLobbyAsync(int lobbyId, ulong userId)
     {
         await using var context = await _factory.CreateDbContextAsync();
-        
+
         var lobby = await context.Lobbies.FindAsync(lobbyId)
                     ?? throw new LobbyNotFoundException();
 
@@ -80,7 +89,7 @@ public class LobbiesService : ILobbiesService
     public async Task CancelLobbyAsync(int lobbyId)
     {
         await using var context = await _factory.CreateDbContextAsync();
-        
+
         var lobby = await context.Lobbies.FindAsync(lobbyId)
                     ?? throw new LobbyNotFoundException();
 
@@ -90,10 +99,48 @@ public class LobbiesService : ILobbiesService
         await context.SaveChangesAsync();
     }
 
-    private async Task UpdateLobbyEmbedAsync(Lobby lobby)
+    private async Task UpdateLobbyEmbedAsync(Lobby lobby, bool cancelled = false)
     {
         var guild = _client.GetGuild(lobby.GuildId);
         var channel = guild.GetTextChannel(lobby.ChannelId);
-        var message = await channel.GetMessageAsync(lobby.MessageId);
+        var message = await channel.GetMessageAsync(lobby.MessageId) as SocketUserMessage;
+
+        if (cancelled)
+        {
+            await message!.ModifyAsync(m =>
+            {
+                m.Content = string.Empty;
+                m.Components = Optional<MessageComponent>.Unspecified;
+                m.Embed = LobbyCancelledEmbed;
+            });
+            return;
+        }
+
+        var players = lobby.JoinedPlayers.Count == 0
+            ? "_No players joined this lobby yet_"
+            : string.Join("\n", lobby.JoinedPlayers.Select(id => $"<@!{id}>"));
+
+        var embed = new EmbedBuilder
+            {
+                Title = "Let's play cards against humanity!",
+                Description = "To join or leave this game, click the button below the message.",
+                ThumbnailUrl = "https://i.imgur.com/Ttu1OPc.png"
+            }
+            .AddField("Game owner", $"<@!{lobby.OwnerId}>")
+            .AddField("Joined players", players)
+            .Build();
+
+        var components = new ComponentBuilder()
+            .WithButton(ButtonBuilder.CreatePrimaryButton("Join / leave this game", $"join:{lobby.Id}"))
+            .WithButton(ButtonBuilder.CreateSecondaryButton("Start game", $"start:{lobby.Id}"))
+            .WithButton(ButtonBuilder.CreateSecondaryButton("Cancel game", $"cancel:{lobby.Id}"))
+            .Build();
+
+        await message!.ModifyAsync(m =>
+        {
+            m.Content = string.Empty;
+            m.Embed = embed;
+            m.Components = components;
+        });
     }
 }
