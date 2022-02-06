079f8cd862780cfbecdf9cd4abdcad85c7dd89b3
Jiří Vrba <jiri@vrba.dev>
2022-02-06 22:06:32 +0100

    Add dev guild ID to discord configuration properties

--- src/CardsAgainstDiscord/Configuration/DiscordConfiguration.cs
+++ src/CardsAgainstDiscord/Configuration/DiscordConfiguration.cs
@@ -4,5 +4,13 @@ public class DiscordConfiguration
 {
     public const string Section = "Discord";
 
+    /// <summary>
+    /// Discord token used for gateway authentication
+    /// </summary>
     public string Token { get; init; } = null!;
+    
+    /// <summary>
+    /// Id of the testing guild to locally register slash commands to
+    /// </summary>
+    public long? GuildId { get; init; } = null;
 }
