c06a05f8059257b7a8acdfeb05c61f1fdd0eb878
Jiří Vrba <jiri@vrba.dev>
2022-02-07 08:24:42 +0100

    Add helper class with color and banner constants

---
+++ src/CardsAgainstDiscord/Discord/DiscordConstants.cs
@@ -0,0 +1,13 @@
+namespace CardsAgainstDiscord.Discord;
+
+public static class DiscordConstants
+{
+    public const int ColorPrimary = 0x5865F2;
+    public const int ColorGreen = 0x57F287;
+    public const int ColorYellow = 0xFEE75C;
+    public const int ColorFuchsia = 0xEB459E;
+    public const int ColorRed = 0xED4245;
+
+    public const string Banner = "https://i.imgur.com/Ttu1OPc.png";
+    public const string BannerInactive = "https://i.imgur.com/JbVGbzw.png";
+}
