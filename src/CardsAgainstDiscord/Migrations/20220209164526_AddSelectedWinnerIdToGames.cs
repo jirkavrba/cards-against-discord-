40887b10fa80d19a0a79c646fdd2eba768c68edf
Jiří Vrba <jiri@vrba.dev>
2022-02-09 17:49:34 +0100

    Whoops

---
+++ src/CardsAgainstDiscord/Migrations/20220209164526_AddSelectedWinnerIdToGames.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardsAgainstDiscord.Migrations
+{
+    public partial class AddSelectedWinnerIdToGames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "selected_winner_id",
+                table: "games",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "selected_winner_id",
+                table: "games");
+        }
+    }
+}
