94902078f4aa3ebef996159336cfeb6c256e960f
Jiří Vrba <jiri@vrba.dev>
2022-02-09 10:41:30 +0100

    Add lobby embed method to the embed builders class

--- src/CardsAgainstDiscord/Discord/EmbedBuilders.cs
+++ src/CardsAgainstDiscord/Discord/EmbedBuilders.cs
@@ -1,3 +1,4 @@
+using CardsAgainstDiscord.Extensions;
 using Discord;
 
 namespace CardsAgainstDiscord.Discord;
@@ -12,4 +13,15 @@ public static class EmbedBuilders
             .WithDescription(description)
             .WithCurrentTimestamp();
     }
+
+    public static Embed LobbyEmbed(ulong ownerId, IEnumerable<ulong> joinedPlayers) =>
+        new EmbedBuilder()
+            .WithColor(DiscordConstants.ColorPrimary)
+            .WithThumbnailUrl(DiscordConstants.Banner)
+            .WithTitle("Let's play cards against humanity!")
+            .WithDescription("To join or leave the game, use the button below.")
+            .AddField("Game owner", ownerId.AsUserMention())
+            .AddField("Joined players", string.Join(", ", joinedPlayers.Select(p => p.AsUserMention())))
+            .WithCurrentTimestamp()
+            .Build();
 }
