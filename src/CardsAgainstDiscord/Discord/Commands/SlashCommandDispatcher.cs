2f547d36cf62cc396d6a97ca149a3cba0b695f4b
Jiří Vrba <jiri@vrba.dev>
2022-02-09 19:50:10 +0100

    Add logging to slash command registration

--- src/CardsAgainstDiscord/Discord/Commands/SlashCommandDispatcher.cs
+++ src/CardsAgainstDiscord/Discord/Commands/SlashCommandDispatcher.cs
@@ -9,13 +9,17 @@ public class SlashCommandDispatcher : ISlashCommandDispatcher
     private readonly IEnumerable<ISlashCommand> _commands;
 
     private readonly DiscordConfiguration _configuration;
+    
     private readonly IHostEnvironment _environment;
 
+    private readonly ILogger<SlashCommandDispatcher> _logger;
+
     public SlashCommandDispatcher(IHostEnvironment environment, IOptions<DiscordConfiguration> configuration,
-        IEnumerable<ISlashCommand> commands)
+        IEnumerable<ISlashCommand> commands, ILogger<SlashCommandDispatcher> logger)
     {
         _environment = environment;
         _commands = commands;
+        _logger = logger;
         _configuration = configuration.Value;
     }
 
@@ -32,9 +36,13 @@ public class SlashCommandDispatcher : ISlashCommandDispatcher
             var id = _configuration.GuildId ?? throw new ArgumentNullException(nameof(_configuration.GuildId));
             var guild = client.GetGuild(id) ?? throw new ArgumentException($"Cannot find the configured guild ({id})!");
 
+            _logger.LogInformation("Registering the commands locally to {guild}", guild.Name);
+            
             await guild.BulkOverwriteApplicationCommandAsync(commands);
             return;
         }
+        
+        _logger.LogInformation("Registering the commands globally");
 
         await client.BulkOverwriteGlobalApplicationCommandsAsync(commands);
     }
